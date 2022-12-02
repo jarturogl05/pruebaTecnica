599c351c048a2bfa1021cc4fc18063a7806fa7c6
José Arturo García López <jarturogl05@gmail.com>
2022-12-01 23:31:03 -0600

    instrucciones

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,5 +11,5 @@ app.use(routes);
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
